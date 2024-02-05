87ab3422516555b5838be018d02ee11a99b0c178
ignacio-marenco-simplisafe <ignacio.marenco@simplisafe.com>
2024-02-05 00:33:28 -0300

    fix

--- utilities/inventory-validate.js
+++ utilities/inventory-validate.js
@@ -1,25 +1,18 @@
 const invValidate = {};
-const { body, validationResult } = require("express-validator")
+const { body, validationResult } = require("express-validator");
 
 const utilities = require("./index");
 
-/**************************
- * New Classification Rules
- ***********************/
 invValidate.classificationRules = () => {
-
   return [
     body("classification_name")
       .trim()
       .isLength({ min: 1 })
       .isAlpha()
-      .withMessage("Please provide a classification name.")
+      .withMessage("Please provide a classification name."),
   ];
 };
 
-/**************************
- *CHecks sent data to make sure its according to the rules.
- ***********************/
 invValidate.checkClassificationData = async (req, res, next) => {
   let errors = [];
   errors = validationResult(req);
@@ -35,66 +28,44 @@ invValidate.checkClassificationData = async (req, res, next) => {
   next();
 };
 
-/**************************
- * New Vehicle Rules
- ***********************/
 invValidate.inventoryRules = () => {
   return [
-    // classification name is required
     body("classification_id")
       .trim()
       .isNumeric({ no_symbols: true })
       .withMessage("Please select a classification name."),
-
-    // vehicle make is required
     body("inv_make")
       .trim()
       .isLength({ min: 3 })
       .withMessage("Please provide vehicle make."),
-
-    // vehicle model is required
     body("inv_model")
       .trim()
       .isLength({ min: 2 })
       .withMessage("Please provide vehicle model."),
-
-    // vehicle description is required
     body("inv_description")
       .trim()
       .isLength({ min: 1 })
       .withMessage("Please provide vehicle description."),
-
-    // vehicle image path is required
     body("inv_image")
       .trim()
       .isLength({ min: 1 })
       .withMessage("Please provide image path"),
-
-    // vehicle thumbnail path is required
     body("inv_thumbnail")
       .trim()
       .isLength({ min: 1 })
       .withMessage("Please provide thumbnail path."),
-
-    // price is required
     body("inv_price")
       .trim()
       .isCurrency()
       .withMessage("Please provide vehicle price."),
-
-    // year is required
     body("inv_year")
       .trim()
       .isInt({ min: 1880, max: 2024 })
       .withMessage("Please provide vehicle year"),
-
-    // miles is required
     body("inv_miles")
       .trim()
       .isInt({ min: 0 })
       .withMessage("Please provide vehicle miles"),
-
-    // color is required
     body("inv_color")
       .trim()
       .isLength({ min: 1 })
@@ -120,7 +91,7 @@ invValidate.checkInventoryData = async (req, res, next) => {
   if (!errors.isEmpty()) {
     let nav = await utilities.getNav();
     let selectList = await utilities.selectList();
-    res.render("./inventory/addVehicle", {
+    res.render("./inventory/add-inventory", {
       errors,
       title: "Add Vehicle",
       nav,
